1586b45dfbdd78b3a83fd77ffe2bc1dc095c8a64
DGOLD7788 <80936904545dgold@gmail.com>
2024-11-14 19:51:00 +0200

    add

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,77 @@
+import css from "./ContactForm.module.css";
+import { ErrorMessage, Formik, Form, Field } from "formik";
+import { nanoid } from "nanoid";
+import { useDispatch } from "react-redux";
+import * as Yup from "yup";
+import { addContact } from "../../redux/contactsSlice";
+
+const phoneRegExp = /^[0-9]{3}-[0-9]{2}-[0-9]{2}$/;
+const ContactValidationSchema = Yup.object().shape({
+  userName: Yup.string()
+    .required("Required")
+    .min(3, "Too short")
+    .max(50, "Too long"),
+  userNumber: Yup.string()
+    .matches(phoneRegExp, "The phone number must match the format 'xxx-xx-xx'")
+    .required("Required"),
+});
+
+const initialValues = {
+  userName: "",
+  userNumber: "",
+};
+
+const ContactForm = () => {
+  const dispatch = useDispatch();
+  const handleSubmit = (values, actions) => {
+    const contactObject = {
+      name: values.userName,
+      number: values.userNumber,
+      id: nanoid(),
+    };
+    dispatch(addContact(contactObject));
+    actions.resetForm();
+  };
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={ContactValidationSchema}
+    >
+      <Form className={css.form}>
+        <label className={css.label}>
+          Name
+          <Field
+            type="text"
+            name="userName"
+            className={css.input}
+            placeholder="Name"
+          />
+          <ErrorMessage
+            className={css.errorText}
+            name="userName"
+            component="span"
+          />
+        </label>
+        <label className={css.label}>
+          Number
+          <Field
+            type="tel"
+            name="userNumber"
+            className={css.input}
+            placeholder="XXX-XX-XX"
+          />
+          <ErrorMessage
+            className={css.errorText}
+            name="userNumber"
+            component="span"
+          />
+        </label>
+        <button type="submit" className={css.button}>
+          Add contact
+        </button>
+      </Form>
+    </Formik>
+  );
+};
+export default ContactForm;
